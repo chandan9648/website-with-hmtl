c862238441e021b3e9f7f7a0c0314dfb37ebc18b
Chandan Chaudhary <166887599+chandan9648@users.noreply.github.com>
2025-02-28 22:10:16 +0530

    Update script.js

--- script.js
+++ script.js
@@ -83,6 +83,7 @@ document.querySelectorAll(".elem").forEach(function (elem) {
     });
   });
 
+
   elem.addEventListener("mousemove", function (dets) {
     var diff = dets.clientY - elem.getBoundingClientRect().top;
     diffrot = dets.clientX - rotate;
